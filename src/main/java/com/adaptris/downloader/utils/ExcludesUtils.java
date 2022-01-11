379a77f60a59038fd6ad5d506f39c5c40eca9f82
Sebastien Belin <sebastien.belin@adaptris.com>
2022-01-11 00:04:21 +0000

    INTERLOK-3872 #comment Remove the possibility of excluding artifact by reading all the jar files in a directory.
    This not needed anymore so this is the easiest fix.

--- src/main/java/com/adaptris/downloader/utils/ExcludesUtils.java
+++ src/main/java/com/adaptris/downloader/utils/ExcludesUtils.java
@@ -1,13 +1,9 @@
 package com.adaptris.downloader.utils;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class ExcludesUtils {
@@ -22,24 +18,8 @@ public class ExcludesUtils {
 
   public static void addToExcludesList(Collection<String> excludesToAdd, List<String> excludes) {
     for (String exclude : excludesToAdd) {
-      File dir = new File(exclude);
-      if (dir.isDirectory()) {
-        excludes.addAll(getExcludesFromDir(dir));
-      } else {
-        excludes.add(exclude);
-      }
+      excludes.add(exclude);
     }
-  }
-
-  private static List<String> getExcludesFromDir(File dir) {
-    List<String> excludes = new ArrayList<>();
-    Collection<File> listFiles = FileUtils.listFiles(dir, new String[] {"jar"}, false);
-    listFiles.forEach(file -> excludes.add(filenameToExclude(file.getName())));
-    return excludes;
-  }
-
-  private static String filenameToExclude(String filename) {
-    return "*:" + FilenameUtils.removeExtension(filename);
   }
 
 }
